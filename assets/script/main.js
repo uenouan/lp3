614b9a1915a25a9b6446879e083e4415ea53307b
uenotakabon <uenotakabon@gmail.com>
2024-06-21 16:18:39 +0900

    updated

---
+++ assets/script/main.js
@@ -0,0 +1,60 @@
+// カルーセル
+$('.voice__slick').slick({
+  // autoplay: true,
+  // autoplaySpeed: 5000,
+  slidesToShow: 3,
+  arrows: false,
+  dots: false,
+  centerMode: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        centerMode: true,
+        centerPadding: "5%",
+      },
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "15%",
+      },
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "10%",
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "5%",
+      },
+    },
+  ],
+});
